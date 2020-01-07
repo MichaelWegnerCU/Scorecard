d7f4a795b50718dc6190e22aa98bf06b0e9ad64c
unknown <miwe7500@colorado.edu>
2020-01-06 19:01:49 -0700

    Updates

---
+++ precache-manifest.7054fa447592abe3e7e73fd78a5e483a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a984a0159a2f892761760d7a1df99286",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/index.html"
+  },
+  {
+    "revision": "6dcf8940650f02ce8747",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8ce61d203cbc1cf97604",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/static/css/main.d9c26589.chunk.css"
+  },
+  {
+    "revision": "6dcf8940650f02ce8747",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/static/js/2.11931f32.chunk.js"
+  },
+  {
+    "revision": "8ce61d203cbc1cf97604",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/static/js/main.e1761843.chunk.js"
+  },
+  {
+    "revision": "5a11b9ea3f6de26b486f",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/static/js/runtime-main.126e75a7.js"
+  },
+  {
+    "revision": "18f755388c205811fb45d0351efa2931",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/static/media/graph1.18f75538.png"
+  },
+  {
+    "revision": "ffdedf8bdf3395e73d34b7fa1fa12afd",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/static/media/graph2.ffdedf8b.png"
+  },
+  {
+    "revision": "eac3de175827474234b0d8fcaadab200",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/static/media/info.eac3de17.svg"
+  },
+  {
+    "revision": "b0879657997c544c349ffcf3fe87ef41",
+    "url": "/MichaelWegnerCU.github.io/Scorecard/static/media/logo.b0879657.PNG"
+  }
+]);
